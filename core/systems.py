1c7368a6160ee8712f2f1a497ad1076297fd7fdf
Matt Palen <matthewmpalen@gmail.com>
2015-07-16 22:01:09 -0400

    Initial

---
+++ core/systems.py
@@ -0,0 +1,23 @@
+# Python
+import logging
+
+# Local
+from .cpu.cpus import CPU
+
+class Intel8080System(object):
+    logging.basicConfig(level=logging.WARNING, filename='logs/py-i8080.py.log', 
+            filemode='w')
+    logger = logging.getLogger('Intel8080System')
+    def __init__(self, filename):
+        self._CPU = CPU()
+
+        try:
+            with open(filename, 'rb') as f:
+                self._data = f.read()
+        except FileNotFoundError as e:
+            Intel8080System.logger.error(e)
+            exit()
+
+    def boot(self):
+        self._CPU.start()
+        Intel8080System.logger.info('Booted system')
