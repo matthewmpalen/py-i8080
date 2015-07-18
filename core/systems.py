d7fca3dbec9e3b396a6271eeb91c577a3289cbd2
Matt Palen <matthewmpalen@gmail.com>
2015-07-18 18:53:07 -0400

    CPU loads file

--- core/systems.py
+++ core/systems.py
@@ -8,12 +8,13 @@ class Intel8080System(object):
     logging.basicConfig(level=logging.WARNING, filename='logs/py-i8080.py.log', 
             filemode='w')
     logger = logging.getLogger('Intel8080System')
+
     def __init__(self, filename):
         self._CPU = CPU()
 
         try:
             with open(filename, 'rb') as f:
-                self._data = f.read()
+                self._CPU.load(f.read())
         except FileNotFoundError as e:
             Intel8080System.logger.error(e)
             exit()
