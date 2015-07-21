e91d55f7f24a93bbeec3c75e7fba5fed67e0edf7
Matt Palen <matthewmpalen@gmail.com>
2015-07-21 16:22:14 -0400

    Methods now return dictionary of condition flags

--- core/cpu/registers.py
+++ core/cpu/registers.py
@@ -1,5 +1,11 @@
-from enum import IntEnum
+# Python
+import logging
 
+from enum import IntEnum, unique
+
+from .flags import zero_bit, sign_bit, carry_bit, parity_bit
+
+@unique
 class RegID(IntEnum):
     A = 0
     B = 1
@@ -17,7 +23,17 @@ class DRegID(IntEnum):
     SP = 7
     PC = 8
 
+def flags(value):
+    return {
+        'z': zero_bit(value), 
+        's': sign_bit(value), 
+        'cy': carry_bit(value), 
+        'p': parity_bit(value)
+    }
+
 class Registers(object):
+    logger = logging.getLogger('Registers')
+
     def __init__(self):
         self._items = bytearray(7)
 
@@ -31,13 +47,31 @@ class Registers(object):
         if value < 0:
             raise ValueError('Must be a positive value')
 
-        self._items[id] += value
+        answer = self._items[id] + value
+        Registers.logger.info(
+            'increment {0}: {1:x} + {2:x} = {3:x}'.format(
+                id.name, self._items[id], value, answer
+            )
+        )
+
+        self.set(id, answer)
+
+        return flags(answer)
 
     def decrement(self, id, value):
         if value < 0:
             raise ValueError('Must be a positive value')
 
-        self._items[id] -= value
+        answer = self._items[id] - value
+        Registers.logger.info(
+            'decrement {0}: {1:x} - {2:x} = {3:x}'.format(
+                id.name, self._items[id], value, answer
+            )
+        )
+
+        self.set(id, answer)
+
+        return flags(answer)
 
     def get_pair(self, id):
         end = id + 2
@@ -54,10 +88,28 @@ class Registers(object):
         if value < 0:
             raise ValueError('Must be a positive value')
 
-        self.set_pair(id, self.get_pair(id) + value)
+        answer = self.get_pair(id) + value
+        Registers.logger.info(
+            'increment_pair {0}: {1:x} + {2:x} = {3:x}'.format(
+                id.name, self.get_pair(id), value, answer
+            )
+        )
+
+        self.set_pair(id, answer)
+
+        return flags(answer)
 
     def decrement_pair(self, id, value):
         if value < 0:
             raise ValueError('Must be a positive value')
 
-        self.set_pair(id, self.get_pair(id) - value)
+        answer = self.get_pair(id) - value
+        Registers.logger.info(
+            'decrement_pair {0}: {1:x} - {2:x} = {3:x}'.format(
+                id.name, self.get_pair(id), value, answer
+            )
+        )
+
+        self.set_pair(id, answer)
+        
+        return flags(answer)
