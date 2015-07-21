5bb6e5be85476d62d7d5908c1ec7eb7139a729b7
Matt Palen <matthewmpalen@gmail.com>
2015-07-21 16:21:29 -0400

    Moved condition flags helpers

--- core/cpu/flags.py
+++ core/cpu/flags.py
@@ -1,3 +1,19 @@
+
+def get_bit(value, index):
+    return (0 != (value & (1 << index)))
+
+def zero_bit(value):
+    return bool(value)
+
+def sign_bit(value):
+    return get_bit(value, 7)
+
+def carry_bit(value):
+    return value < 0x00 or value > 0xff
+
+def parity_bit(value):
+    return (bin(value).count('1') % 2) == 0
+
 class ConditionFlags(object):
     def __init__(self):
         self.s = False
