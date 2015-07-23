d66b4bf36c3bb017900a22080a07a2ba721d49be
Matt Palen <matthewmpalen@gmail.com>
2015-07-22 21:29:42 -0400

    get_bit returns actual bit value rather than boolean

--- core/cpu/flags.py
+++ core/cpu/flags.py
@@ -1,12 +1,11 @@
-
 def get_bit(value, index):
-    return (0 != (value & (1 << index)))
+    return value & (1 << index)
 
 def zero_bit(value):
     return not bool(value)
 
 def sign_bit(value):
-    return get_bit(value, 7)
+    return (0 != get_bit(value, 7))
 
 def carry_bit(value):
     return value < 0x00 or value > 0xff
