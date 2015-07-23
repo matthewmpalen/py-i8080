4704c9cf98d737a49cf7e97bb9c9136e28a117ef
Matt Palen <matthewmpalen@gmail.com>
2015-07-22 21:31:09 -0400

    Added basic TestSuite

--- core/systems.py
+++ core/systems.py
@@ -20,3 +20,23 @@ class Intel8080System(object):
     def boot(self):
         self._CPU.start()
         Intel8080System.logger.info('Booted system')
+
+    def _get_test_suite(self):
+        from unittest import TestSuite, defaultTestLoader
+        import core.cpu.tests as tests1
+
+        suite = TestSuite()
+
+        for t in (tests1, ):
+            suite.addTests(defaultTestLoader.loadTestsFromModule(t))
+
+        return suite
+
+    def run_tests(self):
+        from unittest import TextTestRunner
+
+        Intel8080System.logger.info('Running test suite')
+        suite = self._get_test_suite()
+        TextTestRunner().run(suite)
+
+        Intel8080System.logger.info('Test suite finished')
